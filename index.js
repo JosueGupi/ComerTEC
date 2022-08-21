140dd8ee5692a2f1f43810dc4aeca652789c9494
Josue <josuegupi@gmail.com>
2022-08-20 20:22:08 -0600

    Base of the server

---
+++ index.js
@@ -0,0 +1,31 @@
+const connection = require('./mysql');
+const express = require("express")
+const app = express()
+const userRouter =  require('./routes/Users')
+const storeRouter = require('./routes/Stores')
+const subRouter = require('./routes/Subs')
+const shippingRouter = require('./routes/Shipping')
+const catalogRouter = require('./routes/Catalog')
+const AdminEmployeesRouter = require('./routes/AdminEmployees')
+const AdminProductRouter = require('./routes/AdminProducts')
+const saleRouter = require('./routes/Sales')
+const reviewRouter = require('./routes/Reviews')
+const cors = require('cors')
+
+
+app.use(express.json())
+app.use(cors())
+app.use('/users',userRouter);
+app.use('/stores',storeRouter);
+app.use('/subs',subRouter);
+app.use('/shipping',shippingRouter);
+app.use('/catalog',catalogRouter);
+app.use('/adminEmployees',AdminEmployeesRouter);
+app.use('/adminProduct',AdminProductRouter);
+app.use('/checkout',saleRouter);
+app.use('/review',reviewRouter);
+
+app.listen(3001,()=>{
+    console.log('Servers Run')
+});
+
---
+++ mysql.js
@@ -0,0 +1,13 @@
+
+const mysql = require('mysql2');
+
+
+// Conectamos a la base de datos
+var connection = mysql.createPool({
+    host: process.env.DBHOST || "us-cdbr-east-05.cleardb.net",
+    user: process.env.DBUSER || "b7af72ef51bc8c",
+    password: process.env.DBPASSWORD || "bb6d67f2",
+    database: process.env.DBDATABASE || "heroku_d23e98632d70552"
+  });
+
+module.exports = connection;
