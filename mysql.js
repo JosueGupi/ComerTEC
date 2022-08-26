63ec248a1296600d8fa2934f475a6e697575c9d4
Josue <josuegupi@gmail.com>
2022-08-26 17:12:11 -0600

    Fix Authentication

--- mysql.js
+++ mysql.js
@@ -4,10 +4,10 @@ const mysql = require('mysql2');
 
 // Conectamos a la base de datos
 var connection = mysql.createPool({
-    host: process.env.DBHOST || "us-cdbr-east-05.cleardb.net",
-    user: process.env.DBUSER || "b7af72ef51bc8c",
-    password: process.env.DBPASSWORD || "bb6d67f2",
-    database: process.env.DBDATABASE || "heroku_d23e98632d70552"
+    host: process.env.DBHOST || "us-cdbr-east-06.cleardb.net",
+    user: process.env.DBUSER || "b9775a2b0b345d",
+    password: process.env.DBPASSWORD || "126d9488",
+    database: process.env.DBDATABASE || "heroku_7632f15f2b95b48"
   });
 
 module.exports = connection;
