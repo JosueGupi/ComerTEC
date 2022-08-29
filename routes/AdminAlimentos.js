23445adebccb1216b247105053a5529b0e658b26
Josue <josuegupi@gmail.com>
2022-08-29 12:48:24 -0600

    Eliminar alimento test

--- routes/AdminAlimentos.js
+++ routes/AdminAlimentos.js
@@ -2,13 +2,6 @@ const app = require('express').Router()
 const connection = require('../mysql');
 app.post('/crearAlimento', function (req, res) {
     
-    // connect to your database
-    
-    
-    
-
-        // create Request object
-    
     const nombre = req.body.Nombre
     const cant = req.body.Cantidad
     const tiempo = req.body.TiempoAlimento
@@ -16,6 +9,22 @@ app.post('/crearAlimento', function (req, res) {
     const tipo = req.body.TipoAlimento
     
     connection.query("INSERT INTO `heroku_7632f15f2b95b48`.`alimento`(`Nombre`,`IdTipoAlimento`,`IdTiempo`,`Cantidad`,`Precio`)VALUES('"+nombre+"',"+tipo+","+tiempo+","+cant+","+precio+");", function (error, results) {
+        if (error) throw error;
+        
+        res.json(true);
+      });
+       
+    
+    // query to the database and get the records
+});
+app.post('/eliminarAlimento', function (req, res) {
+    
+    
+    
+    const IdComida = req.body.IdComida
+    
+    
+    connection.query("DELETE FROM `heroku_7632f15f2b95b48`.`alimento`WHERE `alimento`.`idAlimento` = "+IdComida+";", function (error, results) {
         if (error) throw error;
         
         res.json(true);
