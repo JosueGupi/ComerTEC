09c508a87ef61b1d14d3565f3f1a10fc4c0547f6
Josue <josuegupi@gmail.com>
2022-08-29 18:22:49 -0600

    Fix eliminar cliente query

--- routes/AdminClientes.js
+++ routes/AdminClientes.js
@@ -11,7 +11,7 @@ app.post('/actualizarCliente', function (req, res) {
     const correo = req.body.Correo
     const password = req.body.Password
     
-    connection.query("UPDATE `heroku_7632f15f2b95b48`.`personas` SET `Nombre` = '"+nombre+"', `Carnet` ='"+carnet+"', `PrimerApellido` = '"+apellido1+"',`SegundoApellido` = '"+apellido2+"',`FechaNacimiento` = '"+fecha+"',`Correo` = '"+fecha+"', `Password` = '"+password+"' WHERE `Cedula` = "+cedula+";", function (error, results) {
+    connection.query("UPDATE `heroku_7632f15f2b95b48`.`personas` SET `Nombre` = '"+nombre+"', `Carnet` ='"+carnet+"', `PrimerApellido` = '"+apellido1+"',`SegundoApellido` = '"+apellido2+"',`FechaNacimiento` = '"+fecha+"',`Correo` = '"+correo+"', `Password` = '"+password+"' WHERE `Cedula` = "+cedula+";", function (error, results) {
         if (error) {res.json(error);throw error};
         
         res.json(true);
@@ -22,9 +22,9 @@ app.post('/actualizarCliente', function (req, res) {
 });
 app.post('/eliminarCliente', function (req, res) {
     
-    const idCliente = req.params.IdCliente
+    const idCliente = req.body.IdCliente
     
-    connection.query("DELETE FROM `heroku_7632f15f2b95b48`.`personas` WHERE `Cedula` = "+idCliente+";", function (error, results) {
+    connection.query("DELETE FROM `heroku_7632f15f2b95b48`.`personas` WHERE `Cedula` = '"+idCliente+"';", function (error, results) {
         if (error) {res.json(error);throw error};
         
         res.json(true);
