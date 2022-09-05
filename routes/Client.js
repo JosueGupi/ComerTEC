7d03cc4807719c9c55059a4c3083e22921ad50c2
geiisel <montoyageisel@estudiantec.cr>
2022-09-04 23:13:55 -0600

    Query create user

--- routes/Client.js
+++ routes/Client.js
@@ -1,7 +1,7 @@
 const app = require('express').Router();
 const connection = require('../mysql');
 
-app.post('/login', function (req, res) {
+app.get('/login', function (req, res) {
   const password = req.body.Password;
   const user = req.body.User;
   console.log("Data:", password, user);
@@ -24,38 +24,49 @@ app.post('/login', function (req, res) {
 });
 
 app.post('/create', function (req, res) {      
-    const name = req.body.Name;
-    const studentCard = req.body.StudentCard;
-    const id = req.body.Id;
-    const firstSurname = req.body.FirstSurname;
-    const secondSurname = req.body.SecondSurname;
-    const dateBirth = req.body.DateBirth;
-    const email = req.body.Email;
-    const tecPassword = req.body.TecPassword;
-    console.log("Data:", password, user);
-    connection.query(
-      "INSERT INTO `heroku_7632f15f2b95b48`.`personas`(`Nombre`, `Carnet`, `Cedula`, `PrimerApellido`, `SegundoApellido`, " +
-        "`FechaNacimiento`, `Correo`, `Password`, `Admin`) VALUES (" +
-        name + ", " +
-        studentCard + ", " +
-        id + ", " +
-        firstSurname + ", " +
-        secondSurname + ", " +
-        dateBirth + ", " +
-        email + ", " +
-        tecPassword + ", " +
-        0 +
-        " );",
-      function (error, results) {
-          console.log(results)
-        if (error) {
-          res.json(error);
-          throw error;
-        }
-  
-        res.json(results);
+  const name = req.body.Name;
+  const studentCard = req.body.StudentCard;
+  const id = req.body.Id;
+  const firstSurname = req.body.FirstSurname;
+  const secondSurname = req.body.SecondSurname;
+  const dateBirth = req.body.DateBirth;
+  const email = req.body.Email;
+  const tecPassword = req.body.TecPassword;
+  console.log("Query:", "INSERT INTO `heroku_7632f15f2b95b48`.`personas`(`Nombre`, `Carnet`, `Cedula`, `PrimerApellido`, `SegundoApellido`, " +
+  "`FechaNacimiento`, `Correo`, `Password`, `Admin`) VALUES ('" +
+  name + "', " +
+  studentCard + ", " +
+  id + ", '" +
+  firstSurname + "', '" +
+  secondSurname + "', '" +
+  dateBirth + "', '" +
+  email + "', '" +
+  tecPassword + "', " +
+  0 +
+  " );");
+  connection.query(
+    "INSERT INTO `heroku_7632f15f2b95b48`.`personas`(`Nombre`, `Carnet`, `Cedula`, `PrimerApellido`, `SegundoApellido`, " +
+      "`FechaNacimiento`, `Correo`, `Password`, `Admin`) VALUES ('" +
+      name + "', " +
+      studentCard + ", " +
+      id + ", '" +
+      firstSurname + "', '" +
+      secondSurname + "', '" +
+      dateBirth + "', '" +
+      email + "', '" +
+      tecPassword + "', " +
+      0 +
+      " );",
+    function (error, results) {
+        console.log(results)
+      if (error) {
+        res.json(error);
+        throw error;
       }
-    ); 
-  });
+
+      res.json(results);
+    }
+  ); 
+});
 
 module.exports = app;
