c2ee19942a18785494848174504c095b279109db
Josue <josuegupi@gmail.com>
2022-09-09 20:30:20 -0600

    Añadir funciones modificar pedido

--- routes/AdminPedidos.js
+++ routes/AdminPedidos.js
@@ -62,4 +62,47 @@ app.post('/alimentosAgregados', function (req, res) {
   
   // query to the dataase and get the record
 });
+
+app.post('/eliminarAlimentoPedido', function (req, res) {
+    
+  const idPedido = req.body.IdPedido == '' ? "undefined" : req.body.idPedido;
+  const IdAlimento = req.body.IdAlimentoAgregar == '' ? "undefined" : req.body.IdAlimentoEliminar;
+
+  const query = "DELETE FROM `heroku_7632f15f2b95b48`.`pedidoxalimento` WHERE IdPedido = "+idPedido+" AND IdAlimento ="+IdAlimento+" ;";
+  console.log(query);
+  if(query.includes("undefined")){
+    res.json(false);
+  }else{
+    connection.query(query, function (error, results) {
+      if (error) {res.json(error);throw error};
+      
+      res.json(true);
+    });
+  }
+     
+  
+  // query to the dataase and get the record
+});
+app.post('/agregarAlimentoPedido', function (req, res) {
+    
+  const IdAlimento = req.body.IdAlimentoAgregar == '' ? "undefined" : req.body.IdAlimentoAgregar;
+  const idPedido = req.body.IdPedido == '' ? "undefined" : req.body.idPedido;
+  const cantidad = req.body.cantidad == '' ? "undefined" : req.body.Cantidad;
+  
+
+  const query = "INSERT INTO `heroku_7632f15f2b95b48`.`pedidoxalimento`(`IdPedido`,`IdAlimento`,`Cantidad`)VALUES("+idPedido+","+IdAlimento+","+cantidad+");";
+  console.log(query);
+  if(query.includes("undefined")){
+    res.json(false);
+  }else{
+    connection.query(query, function (error, results) {
+      if (error) {res.json(error);throw error};
+      
+      res.json(true);
+    });
+  }
+     
+  
+  // query to the dataase and get the record
+});
 module.exports = app;
